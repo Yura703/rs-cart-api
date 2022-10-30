89453f9dbe5bfd73fc0ddbf2517fb6b4a516b278
Yura Lapitski <lyurik@tut.by>
2022-10-30 23:36:16 +0300

    feat: relocate serverless.yml

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-import * as helmet from 'helmet';
+//import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 4000;
+//const port = process.env.PORT || 4000;
+let server: Handler;
 
-async function bootstrap() {
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-  app.use(helmet());
+  // app.enableCors({
+  //   origin: (req, callback) => callback(null, true),
+  // });
+  // app.use(helmet());
 
-  await app.listen(port);
+  // await app.listen(port);
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+// bootstrap().then(() => {
+//   console.log('App is running on %s port', port);
+// });
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import { IgnorePlugin } from 'webpack';
+
+export default function (options) {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+  ];
+
+  return {
+    ...options,
+    plugins: [
+      ...options.plugins,
+      new IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ],
+    output: {
+      ...options.output,
+      libraryTarget: 'commonjs2',
+    },
+  };
+};
